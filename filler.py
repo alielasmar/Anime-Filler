2ba967e8f74136acbea8ac070838ab3df44ee82d
alielasmar <54985976+alielasmar@users.noreply.github.com>
2021-12-16 18:08:49 -0500

    Add files via upload

--- filler.py
+++ filler.py
@@ -13,141 +13,125 @@ def main():
     getNumbersInArray(fname)                #gets name of file and passes to next function
 
 def getNumbersInArray(fname):               #reads file char by char and appends to list
-    fillerL = []
+    cannonL = []
     count = 0
     with open(fname,'r') as filler:
         for line in filler:
             for character in line:
-                fillerL.append(character)
-    #print(fillerL)
-    deleteSpaces(fillerL)                   
+                cannonL.append(character)
+    #print(cannonL)
+    deleteSpaces(cannonL)                   
 
-def deleteSpaces(fillerL):                  #if current element is a space or new line then delete it
-    for i in range(len(fillerL)):
-        if i < len(fillerL):
-            if fillerL[i] == " ":
-                del fillerL[i]
-            if fillerL[i] == "\n":
-                del fillerL[i]
-    combineCompoundNumbers(fillerL)
+def deleteSpaces(cannonL):                  #if current element is a space or new line then delete it
+    for i in range(len(cannonL)):
+        if i < len(cannonL):
+            if cannonL[i] == " ":
+                del cannonL[i]
+            if cannonL[i] == "\n":
+                del cannonL[i]
+    combineCompoundNumbers(cannonL)
 
-def combineCompoundNumbers(fillerL):        #checks to see if current and next element in array are ints
-    for i in range(len(fillerL)):
-        if i <= len(fillerL) -2:
-            if fillerL[i] != ',' and fillerL[i] != '-':
-                if fillerL[i+1] != ',' and fillerL[i+1] != '-' and fillerL[i+2] != ',' and fillerL[i+2] != '-':
-                    tmp = fillerL[i] + fillerL[i+1] + fillerL[i+2]
-                    del fillerL[i+2]
-                    del fillerL[i+1]
-                    del fillerL[i]
-                    fillerL.insert(i,tmp)   #insert at current element, placement matters because of dashes
-                elif fillerL[i+1] != ',' and fillerL[i+1] != '-':
-                    tmp = fillerL[i] + fillerL[i+1]
-                    del fillerL[i+1]
-                    del fillerL[i]
-                    fillerL.insert(i,tmp)
+def combineCompoundNumbers(cannonL):        #checks to see if current and next element in array are ints
+    for i in range(len(cannonL)):
+        if i <= len(cannonL) -2:
+            if cannonL[i] != ',' and cannonL[i] != '-':
+                if cannonL[i+1] != ',' and cannonL[i+1] != '-' and cannonL[i+2] != ',' and cannonL[i+2] != '-':
+                    tmp = cannonL[i] + cannonL[i+1] + cannonL[i+2]
+                    del cannonL[i+2]
+                    del cannonL[i+1]
+                    del cannonL[i]
+                    cannonL.insert(i,tmp)   #insert at current element, placement matters because of dashes
+                elif cannonL[i+1] != ',' and cannonL[i+1] != '-':
+                    tmp = cannonL[i] + cannonL[i+1]
+                    del cannonL[i+1]
+                    del cannonL[i]
+                    cannonL.insert(i,tmp)
                     
                     #print(tmp)
-    #print(fillerL)
-    deleteCommas(fillerL)
+    #print(cannonL)
+    deleteCommas(cannonL)
 
 
-def deleteCommas(fillerL):                  #if an element is a comma it's deleted
-    #print(len(fillerL))
-    for i in range(len(fillerL)):
-        if i < len(fillerL):
-            if fillerL[i] == ',':
-                del fillerL[i]
-    #print(fillerL)
-    convertStrToInt(fillerL)
+def deleteCommas(cannonL):                  #if an element is a comma it's deleted
+    #print(len(cannonL))
+    for i in range(len(cannonL)):
+        if i < len(cannonL):
+            if cannonL[i] == ',':
+                del cannonL[i]
+    #print(cannonL)
+    convertStrToInt(cannonL)
 
 
-def convertStrToInt(fillerL):           # converts str to int
-    for i in range(len(fillerL)):
-        if fillerL[i] != '-':
-            fillerL[i] = int(fillerL[i])
-    print(fillerL,end="\n \n \n")
-    addBetweenDashes(fillerL)
+def convertStrToInt(cannonL):           # converts str to int
+    for i in range(len(cannonL)):
+        if cannonL[i] != '-':
+            cannonL[i] = int(cannonL[i])
+    print(cannonL,end="\n \n \n")
+    addBetweenDashes(cannonL)
 
 
-def addBetweenDashes(fillerL):          # if there is a dash, store element before and after, iterate between them and append to list
-    maximum = checkMax(fillerL)
-    minimum = fillerL[0]
-    for i in range(len(fillerL)):
-        if fillerL[i] == '-':
+def addBetweenDashes(cannonL):          # if there is a dash, store element before and after, iterate between them and append to list
+    maximum = checkMax(cannonL)
+    minimum = cannonL[0]
+    for i in range(len(cannonL)):
+        if cannonL[i] == '-':
             count = 0
-            before = fillerL[i-1]
-            after = fillerL[i+1]
+            before = cannonL[i-1]
+            after = cannonL[i+1]
             while before < after-1:
                 before = before + 1
-                #print('before: ',end=" ")
-                #print(before)
-                if not before < minimum:
-                    #print(not before < minimum, end = ",")
-                    #print('before: ',end=" ")
-                    #print(before)
-                    fillerL.append(before)
-    #print(fillerL)
-    delDashes(fillerL)
+                cannonL.append(before)
+    #print(cannonL)
+    delDashes(cannonL)
                     
-def delDashes(fillerL):             #Deletes dashes found in array
-    for i in range(len(fillerL)):
-        if(i < len(fillerL)):
-            if fillerL[i] == "-":
-                fillerL.remove("-")
-    print(fillerL)
-    #sortList(fillerL)
-
-#DISABLED OTHER FUNCTIONS TEMPORARILY
+def delDashes(cannonL):             #Deletes dashes found in array
+    for i in range(len(cannonL)):
+        if(i < len(cannonL)):
+            if cannonL[i] == "-":
+                cannonL.remove("-")
+    #print(cannonL)
+    sortList(cannonL)
 
 
+def sortList(cannonL):
+    cannonL.sort()
+    print(cannonL)
+    userInput(cannonL)
 
 
-
-
-
-
-
-
-
-
-
-def sortList(fillerL):
-    fillerL.sort()
-    print(fillerL)
-    userInput(fillerL)
-
-
-def userInput(fillerL):
+def userInput(cannonL):
     error = False
     curEp = int(input('enter in # episode you are on:'))
-    maximum = checkMax(fillerL)
-    print( curEp in fillerL and error == True)
+    maximum = checkMax(cannonL)
     
     if curEp > maximum:
         print("The episode entered is not in list of episodes")
         error = True
+
+    if curEp < 0:
+        error = True
+        print("Please enter in a positive number")
         
-    elif curEp in fillerL and error == False:
-        print("episode found")
-        curPos = fillerL.index(curEp)
-        epLeft = len(fillerL) - curPos
+    elif curEp in cannonL and error == False:
+        curPos = cannonL.index(curEp)
+        epLeft = len(cannonL) - curPos
         print("episodes left", end = " ")
         print (epLeft)
         
-    elif curEp < maximum and error == False and curEp not in fillerL:
-        print("searching for episode")
-        while curEp not in fillerL:
+    elif curEp < maximum and error == False and curEp not in cannonL:
+        while curEp not in cannonL:
             curEp = curEp +1
-        curPos = fillerL.index(curEp)
-        epLeft = len(fillerL) - curPos
+        curPos = cannonL.index(curEp)
+        epLeft = len(cannonL) - curPos
         print("episodes left", end = " ")
         print (epLeft)
+    if error == True:
+        userInput(cannonL)
 
 
-def checkMax(fillerL):
-    indx = len(fillerL)-1
-    maximum = fillerL[indx]
+def checkMax(cannonL):
+    indx = len(cannonL)-1
+    maximum = cannonL[indx]
     return maximum
         
     
